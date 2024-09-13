b39f4ee279113171cde0e812eb698bbfddf7b6b9
dinhvandong <dongvan.ict3@gmail.com>
2024-09-13 13:01:16 +0700

    first commit

---
+++ src/main/java/com/huce/shopee_online/ShopeeOnlineApplication.java
@@ -0,0 +1,13 @@
+package com.huce.shopee_online;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopeeOnlineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopeeOnlineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/huce/shopee_online/ShopeeOnlineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.huce.shopee_online;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopeeOnlineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
